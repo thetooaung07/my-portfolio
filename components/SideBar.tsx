e3395648d1fd07f75e08d325f7d9a262fa5f94aa
thetooaung07 <thetooaung07@gmail.com>
2021-09-06 23:40:07 +0630

    Initial Setup with Typescript Tailwind css

---
+++ components/SideBar.tsx
@@ -0,0 +1,14 @@
+const SideBar = () => {
+  return (
+    <div>
+      <img src="images/akame_cropped.jpg" alt="Profile" />
+
+      <h3>
+        <span> Thet Oo</span> Aung
+      </h3>
+      <p>Download Resume</p>
+    </div>
+  );
+};
+
+export default SideBar;
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+const index = () => {
+  return (
+    <div>
+      <h1>Bye Bye World</h1>
+    </div>
+  )
+}
+
+export default index
