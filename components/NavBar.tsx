0cbb4d746a90ff900dea07a2430b25f93c9378bb
thetooaung07 <thetooaung07@gmail.com>
2021-09-13 00:48:33 +0630

    NavBar for Content Page

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,35 +1,67 @@
-import { useState } from "react";
+import { FunctionComponent, useEffect, useState } from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
+
+const NavItem: FunctionComponent<{
+  activeItem: string;
+  setActiveItem: Function;
+  name: string;
+  route: string;
+}> = ({ activeItem, name, route, setActiveItem }) => {
+  return activeItem !== name ? (
+    <Link href={route}>
+      <a>
+        <span className="text-black hover:text-green"
+          onClick={() => {
+            setActiveItem(name);
+          }}
+        >
+          {name}
+        </span>
+      </a>
+    </Link>
+  ) : null;
+};
+
 const NavBar = () => {
   const [activeItem, setActiveItem] = useState<string>("");
 
+  const { pathname } = useRouter();
+
+  useEffect(() => {
+    if (pathname === "/") {
+      setActiveItem("About");
+    }
+    if (pathname === "/projects") {
+      setActiveItem("Projects");
+    }
+    if (pathname === "/resume") {
+      setActiveItem("Resume");
+    }
+  }, [pathname]);
+
   return (
-    <div>
-      <span className="font-bold text-green">{activeItem}</span>
-      <div className="flex space-x-3 text-red-400 font-lg">
-        {activeItem !== "About" && (
-          <Link href="/">
-            <a>
-              <span onClick={()=> {setActiveItem('About')}} >About</span>
-            </a>
-          </Link>
-        )}
-
-        {activeItem !== "Projects" && (
-          <Link href="/projects">
-            <a>
-              <span onClick={()=> {setActiveItem('Projects')}} >Projects</span>
-            </a>
-          </Link>
-        )}
-
-        {activeItem !== "Resume" && (
-          <Link href="/resume">
-            <a>
-              <span onClick={()=> {setActiveItem('Resume')}} >Resume</span>
-            </a>
-          </Link>
-        )}
+    <div className="flex justify-between px-5 py-3 my-3">
+      <span className="text-xl font-bold border-b-4 text-green border-green md:text-2xl ">{activeItem}</span>
+      <div className="flex space-x-3 text-lg text-red-400">
+        <NavItem
+          activeItem={activeItem}
+          setActiveItem={setActiveItem}
+          name="About"
+          route="/"
+        />
+        <NavItem
+          activeItem={activeItem}
+          setActiveItem={setActiveItem}
+          name="Projects"
+          route="/projects"
+        />
+        <NavItem
+          activeItem={activeItem}
+          setActiveItem={setActiveItem}
+          name="Resume"
+          route="/resume"
+        />
       </div>
     </div>
   );
